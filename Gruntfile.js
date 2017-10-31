7652ffd1166a10b86b5f48015b04263d2d563cde
Adam Youngers <a_youngers@hotmail.com>
2017-10-31 10:31:53 -0700

    Uploading basic setup files

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+/**
+ * Created by adamyoungers on 10/31/17.
+ */
+module.exports = function (grunt) {
+
+
+
+    // grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-contrib-clean');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
+    // grunt.loadNpmTasks('grunt-sass');
+    // grunt.loadNpmTasks('grunt-contrib-copy');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt.loadNpmTasks('grunt-mocha');
+
+};
